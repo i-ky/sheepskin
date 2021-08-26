7aa80303ac198a5efa74d719fd8fa7ffc548fcd4
i-ky <gl.ivanovsky@gmail.com>
2021-08-26 18:54:00 +0000

    Add few tests

--- tests/test_compatibility.py
+++ tests/test_compatibility.py
@@ -4,6 +4,9 @@ from zabbix import get, agent, agent2
 @mark.parametrize("key", [
     "agent.ping",
     "web.certificate.get",
+    "web.certificate.get[1,2,3,4]",
+    "web.certificate.get[zabbix.com]",
+    "web.certificate.get[example.org]",
 ])
 def test_compatibility(key):
     assert get(key, agent) == get(key, agent2)
